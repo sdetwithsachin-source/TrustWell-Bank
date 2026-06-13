2f542a16628e68155906125872323dd1fb7fc7a6
sdetwithsachin-source <sdetwithsachin@gmail.com>
2026-06-13 15:16:52 +0530

    Added Money-button in Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ function Dashboard() {
         <div className="dashboard-container">
             <button
                 id="transferBtn"
-                className="payment-button"
+                className="Money-button"
                 data-testid="transfer-btn">
 
                 Transfer Money
