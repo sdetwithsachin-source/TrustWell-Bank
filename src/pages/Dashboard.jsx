5a600543d3491cea09ec8ba7546c59685c7e65c9
sdetwithsachin-source <sdetwithsachin@gmail.com>
2026-06-14 16:07:35 +0530

    Added transferBtnAutoHealed in Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ function Dashboard() {
     return (
         <div className="dashboard-container">
             <button
-                id="transferBtnNew6"
+                id="transferBtnAutoHealed"
                 className="payment-button"
                 data-testid="transfer-btn">
 
