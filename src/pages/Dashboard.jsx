a3460a0bfb012a3ea2344366335183927afa14d8
sdetwithsachin-source <sdetwithsachin@gmail.com>
2026-06-14 12:43:36 +0530

    Added transfer-btn5 in Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ function Dashboard() {
     return (
         <div className="dashboard-container">
             <button
-                id="transfer-btn4"
+                id="transfer-btn5"
                 className="payment-button"
                 data-testid="transfer-btn">
 
