4f558a392f8d8c0218cdfc4b0b7273ea8eb94e1b
sdetwithsachin-source <sdetwithsachin@gmail.com>
2026-06-14 11:25:11 +0530

    Added submit-payment-btn in Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ function Dashboard() {
     return (
         <div className="dashboard-container">
             <button
-                id="payment-button"
+                id="submit-payment-btn"
                 className="payment-button"
                 data-testid="transfer-btn">
 
