22b2eb955fb72b1693f09f92d23894191000b8c0
sdetwithsachin-source <sdetwithsachin@gmail.com>
2026-06-27 22:15:36 +0530

    Added transferBtnAutoHealedv4 in Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ function Dashboard() {
     return (
         <div className="dashboard-container">
             <button
-                id="transferBtnAutoHealedv3"
+                id="transferBtnAutoHealedv4"
                 className="payment-button"
                 data-testid="transfer-btn">
 
